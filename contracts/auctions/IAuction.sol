ee3b0cf13604aae61033c0f2beef3eb37d16b2eb
Michael Ruzavin <misharuz25@gmail.com>
2022-12-08 18:01:22 +0300

    Fix license

--- contracts/auctions/IAuction.sol
+++ contracts/auctions/IAuction.sol
@@ -8,4 +8,9 @@ interface IAuction {
     function endDateTime() external view returns (uint);
     function isEnded() external view returns (bool);
     function isStarted() external view returns (bool);
+    function startDateTime() external view returns (uint);
+
+    function bid() external payable;
+    function getHighestBid() external view returns (uint);
+    function getHighestBidder() external view returns (address);
 }
